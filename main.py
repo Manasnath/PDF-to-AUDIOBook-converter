e99467359f531f709684ae4ef594c72897e11270
Manas Nath <75411049+Manasnath@users.noreply.github.com>
2021-02-21 10:49:06 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+import pyttsx3
+import PyPDF2
+
+book = open('2002_Bookmatter_AppliedChemistry.pdf.','rb')
+pdfReader = PyPDF2.PdfFileReader(book)
+pages = pdfReader.numPages
+print(pages)
+speaker = pyttsx3.init()
+for num in range(22,pages):
+    page = pdfReader.getPage(22)
+    text = page.extractText()
+    speaker.say(text)
+    speaker.runAndWait()
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
